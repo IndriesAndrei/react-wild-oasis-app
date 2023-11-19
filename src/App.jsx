5c05de023b07b65a5141a4aa6a86e0ce8e995eca
Indries Andrei <indriesandrei@yahoo.co.uk>
2023-11-19 16:02:41 +0200

    Install Styled Components library and style components with it

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Input from "./ui/Input";
+import Heading from "./ui/Heading";
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;  
+`;
+
 export default function App() {
-  return <div>Hello</div>
+  return (
+    <>
+      {/* apply our GlobalStyles from GlobalStyles.js file (this does not accept children components) */}
+      <GlobalStyles />
+      <StyledApp>
+        {/* we can also send props to our Styled Components */}
+        <Heading as="h1">The Wild Oasis</Heading>
+        <Heading as="h2">Check in and out</Heading>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+
+        <Heading as="h3">Form</Heading>
+        <Input type="number" placeholder="Number of guests" />
+      </StyledApp>
+    </>
+  );
 }
